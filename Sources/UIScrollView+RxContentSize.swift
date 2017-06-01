fbf9a22a266ba2dac8b8f19ddb9e4bf61157f7d9
Amit Jain <gravicle@gmail.com>
2017-06-01 10:56:38 -0700

    Fix keypath in UIScrollView+RxContentSize

--- Sources/UIScrollView+RxContentSize.swift
+++ Sources/UIScrollView+RxContentSize.swift
@@ -5,7 +5,7 @@ import RxCocoa
 extension Reactive where Base == UITableView {
 
     public var contentSize: Driver<CGSize> {
-        return observe(CGSize.self, "base.contentSize")
+        return observe(CGSize.self, #keyPath(UITableView.contentSize))
             .replaceNil(with: .zero)
             .asDriver(onErrorJustReturn: .zero)
     }
